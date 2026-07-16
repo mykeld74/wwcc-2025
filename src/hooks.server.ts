ee8945109fbf4b380ca4636e95d849872bc8252c
mykeld74 <mike@msdwebsolutions.com>
2026-07-16 10:21:50 -0600

    remove sentry test routes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,21 +1,10 @@
-import {sequence} from '@sveltejs/kit/hooks';
+import './instrumentation.server';
+import { sequence } from '@sveltejs/kit/hooks';
 import * as Sentry from '@sentry/sveltekit';
 import { auth } from '$lib/server/auth';
 import { svelteKitHandler } from 'better-auth/svelte-kit';
 import { building } from '$app/environment';
 import type { Handle } from '@sveltejs/kit';
-
-Sentry.init({
-    dsn: "https://022115c28797e3b17edea6b4a442eaf1@o4511745769013248.ingest.us.sentry.io/4511745774387200",
-    tracesSampleRate: 1,
-    enableLogs: true,
-    dataCollection: {
-      // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
-      // https://docs.sentry.io/platforms/javascript/guides/sveltekit/configuration/options/#dataCollection
-      // userInfo: false,
-      // httpBodies: [],
-    },
-})
 
 export const handle: Handle = sequence(Sentry.sentryHandle(), async ({ event, resolve }) => {
 	const session = await auth.api.getSession({
@@ -32,4 +21,5 @@ export const handle: Handle = sequence(Sentry.sentryHandle(), async ({ event, re
 
 	return svelteKitHandler({ event, resolve, auth, building });
 });
+
 export const handleError = Sentry.handleErrorWithSentry();
