e143af69bc3b9475b063d9f0897e48b77f09c783
mykeld74 <mike@msdwebsolutions.com>
2026-07-16 09:57:38 -0600

    add sentry

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,23 @@
+import {sequence} from '@sveltejs/kit/hooks';
+import * as Sentry from '@sentry/sveltekit';
 import { auth } from '$lib/server/auth';
 import { svelteKitHandler } from 'better-auth/svelte-kit';
 import { building } from '$app/environment';
 import type { Handle } from '@sveltejs/kit';
 
-export const handle: Handle = async ({ event, resolve }) => {
+Sentry.init({
+    dsn: "https://022115c28797e3b17edea6b4a442eaf1@o4511745769013248.ingest.us.sentry.io/4511745774387200",
+    tracesSampleRate: 1,
+    enableLogs: true,
+    dataCollection: {
+      // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+      // https://docs.sentry.io/platforms/javascript/guides/sveltekit/configuration/options/#dataCollection
+      // userInfo: false,
+      // httpBodies: [],
+    },
+})
+
+export const handle: Handle = sequence(Sentry.sentryHandle(), async ({ event, resolve }) => {
 	const session = await auth.api.getSession({
 		headers: event.request.headers
 	});
@@ -17,4 +31,5 @@ export const handle: Handle = async ({ event, resolve }) => {
 	}
 
 	return svelteKitHandler({ event, resolve, auth, building });
-};
+});
+export const handleError = Sentry.handleErrorWithSentry();
